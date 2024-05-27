537ea27d20ef51ef3eee5afc8bb8ddd3b2f61dbd
jedrzejbiernacki <jedrx.aegirsson@gmail.com>
2024-05-27 20:32:42 +0200

    settery i gettery dla Soldiera

--- src/Simulation.java
+++ src/Simulation.java
@@ -10,4 +10,41 @@ abstract class Soldier {
     private int movement;
     private int range;
 
+    private int x_position;
+    private int y_position;
+
+    private void setHealth(int newHealth) {
+        this.health = newHealth;
+    }
+    private void setPower(int newPower) {
+        this.power = newPower;
+    }
+    private void setMovement(int newMovement) {
+        this.movement = newMovement;
+    }
+    private void setRange(int newRange) {
+        this.range = newRange;
+    }
+    private void setX_position(int newX_position) {
+        this.x_position = x_position;
+    }
+    private void setY_position(int newY_position) {
+        this.y_position = newY_position;
+    }
+    private int getHealth() {
+        return this.health;
+    }
+    private int getPower() {
+        return this.power;
+    }
+    private int getMovement() {
+        return this.movement;
+    }
+    private int getRange() {
+        return this.range;
+    }
+}
+
+class Knight extends Soldier{
+
 }
