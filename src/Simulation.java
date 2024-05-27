90c7338c5b74edf2f46a54d6517181ef61698062
jedrzejbiernacki <jedrx.aegirsson@gmail.com>
2024-05-27 13:44:19 +0200

    Initial commit

---
+++ src/Simulation.java
@@ -0,0 +1,13 @@
+public class Simulation {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+abstract class Soldier {
+    private int health;
+    private int power;
+    private int movement;
+    private int range;
+
+}
