c2f7bdd409702d830024a500bcec3d62e47cee1c
jedrzejbiernacki <jedrx.aegirsson@gmail.com>
2024-05-27 21:00:57 +0200

    klasa abstrakcyjna field i dodane 2 klasy dziedziczace Mud i Field

--- src/Simulation.java
+++ src/Simulation.java
@@ -52,11 +52,13 @@ abstract class Soldier {
 }
 
 class Knight extends Soldier{
-    public Knight() {
+    public Knight(int x, int y) {
         this.setPower(27);
         this.setHealth(130);
         this.setMovement(5);
         this.setRange(2);
+        this.setX_position(x);
+        this.setY_position(y);
     }
 
     public void attack(Soldier enemy) {
@@ -64,11 +66,13 @@ class Knight extends Soldier{
     }
 }
 class Archer extends Soldier{
-    public Archer() {
+    public Archer(int x, int y) {
         this.setPower(17);
         this.setHealth(100);
         this.setMovement(6);
         this.setRange(10);
+        this.setX_position(x);
+        this.setY_position(y);
     }
     public void attack(Soldier enemy) {
         enemy.setHealth(enemy.getHealth()-this.getPower());
@@ -76,13 +80,63 @@ class Archer extends Soldier{
 
 }
 class Horseman extends Soldier{
-    public Horseman() {
+    public Horseman(int x, int y) {
         this.setPower(37);
         this.setHealth(150);
         this.setMovement(10);
         this.setRange(3);
+        this.setX_position(x);
+        this.setY_position(y);
     }
     public void attack(Soldier enemy) {
         enemy.setHealth(enemy.getHealth()-this.getPower());
+    }
+}
+
+abstract class Field {
+    private boolean blocks;
+    private int x_position;
+    private int y_position;
+    private double movement_modifier;
+    protected void setBlocks(boolean blocks) {
+        this.blocks = blocks;
+    }
+    protected void setX_position(int x_position) {
+        this.x_position = x_position;
+    }
+    protected void setY_position(int y_position) {
+        this.y_position = y_position;
+    }
+    protected void setMovement_modifier(double modifier) {
+        this.movement_modifier = modifier;
+    }
+    protected int getX_position() {
+        return this.x_position;
+    }
+    protected int getY_position() {
+        return this.y_position;
+    }
+    protected boolean getBlocks() {
+        return blocks;
+    }
+    protected double getMovement_modifier() {
+        return this.movement_modifier;
+    }
+}
+
+class Rocks extends Field {
+    Rocks(int x, int y) {
+        this.setBlocks(true);
+        this.setX_position(x);
+        this.setY_position(y);
+        this.setMovement_modifier(0);
+    }
+}
+class Mud extends Field {
+    Mud(int x, int y) {
+        this.setBlocks(false);
+        this.setX_position(x);
+        this.setY_position(y);
+        this.setMovement_modifier(0.5);
     }
 }
